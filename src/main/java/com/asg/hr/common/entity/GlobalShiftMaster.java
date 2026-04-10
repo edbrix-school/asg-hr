76907e51195846ca61a9c6534762ae5b87729b2e
Vaikundamuthu <vkannan@hexalytics.com>
2026-04-10 17:14:53 +0530

    Removed hardcoded schema

--- src/main/java/com/asg/hr/common/entity/GlobalShiftMaster.java
+++ src/main/java/com/asg/hr/common/entity/GlobalShiftMaster.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "GLOBAL_SHIFT_MASTER", schema = "PRODUCTION")
+@Table(name = "GLOBAL_SHIFT_MASTER")
 public class GlobalShiftMaster {
 
     @Id
