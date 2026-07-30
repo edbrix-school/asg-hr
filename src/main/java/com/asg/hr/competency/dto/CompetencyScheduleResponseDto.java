1b702d1617d3a744627fb302c24f093a580b6d8f
Sahil Khurana <skhurana@hexalytics.com>
2026-07-30 16:44:56 +0530

    emp-per-rev-sch-created-by-date-issue

--- src/main/java/com/asg/hr/competency/dto/CompetencyScheduleResponseDto.java
+++ src/main/java/com/asg/hr/competency/dto/CompetencyScheduleResponseDto.java
@@ -1,12 +1,14 @@
 package com.asg.hr.competency.dto;
 
 import com.asg.common.lib.dto.LovGetListDto;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -22,4 +24,14 @@ public class CompetencyScheduleResponseDto {
     private String active;
     private LocalDate evaluationDate;
     private LovGetListDto scheduleDtl;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime createdDate;
+
+    private String createdBy;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime modifiedDate;
+
+    private String modifiedBy;
 }
